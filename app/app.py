592790c2a9cb3691253f15ac225ff286d7fb53c9
Wojciech Gruszka <wojciech@gruszka.eu>
2020-05-16 17:42:34 +0200

    Fixed most error for review

---
+++ app/app.py
@@ -0,0 +1,44 @@
+from flask import Flask
+from flask_jwt_extended import JWTManager
+
+
+def create_app(db_uri, keys_dir_path):
+    app = Flask(__name__)
+
+    private_key = ''
+    public_key = ''
+    with open(keys_dir_path + '/jwtRS256.key', 'r') as content_file:
+        private_key = content_file.read()
+
+    with open(keys_dir_path + '/jwtRS256.key.pub', 'r') as content_file:
+        public_key = content_file.read()
+
+    app.config['JWT_ALGORITHM'] = 'RS256'
+    app.config['JWT_PUBLIC_KEY'] = public_key
+    app.config['JWT_PRIVATE_KEY'] = private_key
+    app.config['JWT_IDENTITY_CLAIM'] = 'sub'
+    app.config['JWT_USER_CLAIMS'] = 'payload'
+    app.config['JWT_CLAIMS_IN_REFRESH_TOKEN'] = False
+    app.config['JWT_ERROR_MESSAGE_KEY'] = 'message'
+    app.config['JWT_TOKEN_LOCATION'] = ('headers', 'cookies')
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    from extensions import (
+        api,
+        jwt,
+        db,
+    )
+
+    jwt.init_app(app)
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
+
+    import resources
+    resources.BaseResource.register(api)
+    api.init_app(app)
+
+    return app
--- app/resources.py
+++ app/resources.py
@@ -1,89 +1,135 @@
-import hashlib
-from flask_restful import Resource, reqparse, abort
-from flask_jwt_extended import (create_access_token, create_refresh_token, jwt_required, jwt_refresh_token_required, get_jwt_identity, get_raw_jwt)
-from main import user_storage, jwt, public_key
 from models import User
+from extensions import (
+    jwt,
+    db,
+)
+
+import hashlib
+from typing import (
+    MutableMapping,
+    Type,
+)
+from flask_restful import (
+    Resource,
+    reqparse,
+    abort,
+)
+from flask_jwt_extended import (
+    create_access_token,
+    create_refresh_token,
+    jwt_required,
+    jwt_refresh_token_required,
+    get_jwt_identity,
+    get_raw_jwt
+)
 
 
 @jwt.user_claims_loader
-def add_claims_to_access_token(user):
+def add_claims_to_access_token(user: User):
     return {
         'username': user.username,
     }
 
+
 @jwt.user_identity_loader
-def user_identity_lookup(user):
+def user_identity_lookup(user: User):
     return user.id
 
-class UserRegistration(Resource):
+
+class BaseResource(Resource):
+    __resources: MutableMapping[str, Type[Resource]] = {}
+
+    def __init_subclass__(cls, *args, **kwargs):
+        cls.__resources[cls.path] = cls
+        super().__init_subclass__(*args, **kwargs)
+
+    @classmethod
+    def register(cls, api):
+        for path, res in cls.__resources.items():
+            api.add_resource(res, path)
+
+
+class UserRegistration(BaseResource):
+    path = "/v1/register"
+
     def __init__(self):
         self.parser = reqparse.RequestParser()
-        self.parser.add_argument('username', help = 'This field cannot be blank', required = True)
-        self.parser.add_argument('password', help = 'This field cannot be blank', required = True) 
-        self.parser.add_argument('email', help = 'This field cannot be blank', required = True) 
-        
+        self.parser.add_argument(
+            'username', help='This field cannot be blank', required=True)
+        self.parser.add_argument(
+            'password', help='This field cannot be blank', required=True)
+        self.parser.add_argument(
+            'email', help='This field cannot be blank', required=True)
+
     def post(self):
         data = self.parser.parse_args()
         username = data['username']
         password = data['password']
         email = data['email']
-        passwordHash = hashlib.sha256(password.encode('utf-8')).hexdigest()
-        user = User(username, passwordHash, email)
-        user_storage.add_user(user)
+        password_hash = hashlib.sha256(password.encode('utf-8')).hexdigest()
+        user = User(username=username,
+                    password_hash=password_hash,
+                    email=email)
+        db.session.add(user)
+        db.session.commit()
         return {'message': 'ok'}
 
 
-class UserLogin(Resource):
+class UserLogin(BaseResource):
+    path = "/v1/login"
+
     def __init__(self):
         self.parser = reqparse.RequestParser()
-        self.parser.add_argument('username', help = 'This field cannot be blank', required = True)
-        self.parser.add_argument('password', help = 'This field cannot be blank', required = True) 
+        self.parser.add_argument(
+            'username', help='This field cannot be blank', required=True)
+        self.parser.add_argument(
+            'password', help='This field cannot be blank', required=True)
 
     def post(self):
         data = self.parser.parse_args()
         username = data['username']
         password = data['password']
-        passwordHash = hashlib.sha256(password.encode('utf-8')).hexdigest()
-        
-        user = user_storage.get_user_by_credentials(username, passwordHash)
-        if user == None:
+        password_hash = hashlib.sha256(password.encode('utf-8')).hexdigest()
+
+        user = User.query.filter_by(username=username).first()
+        if user is None or user.password_hash != password_hash:
             abort(403, message="invalid credentials")
 
         result = {
             'accessToken': create_access_token(user, fresh=True),
             'refreshToken': create_refresh_token(user)
         }
-        return result  
-      
-class TokenRefresh(Resource):
+        return result
+
+
+class TokenRefresh(BaseResource):
+    path = "/v1/refresh-token"
+
     @jwt_refresh_token_required
     def post(self):
         user_id = get_jwt_identity()
-        user = user_storage.get_user_by_id(user_id)
-        if user == None:
+        user = User.query.filter_by(id=user_id).first()
+        if user is None:
             abort(403)
 
         result = {
             'accessToken': create_access_token(user, fresh=False),
             'refreshToken': create_refresh_token(user)
         }
-        return result  
+        return result
 
-class PublicKey(Resource):
-    def get(self):
-        key = public_key
-        key = key.replace('-----BEGIN PUBLIC KEY-----', '').replace('-----END PUBLIC KEY-----', '').replace('\n', '')
-        return {'key': key}  
 
-class UserMe(Resource):
+class UserMe(BaseResource):
+    path = "/v1/me"
+
     @jwt_required
     def get(self):
         user_id = get_jwt_identity()
-        user = user_storage.get_user_by_id(user_id)
-        if user == None:
+        user = User.query.filter_by(id=user_id).first()
+        if user is None:
             abort(403)
 
         return {
             'username': user.username,
             'email': user.email
-        }  
+        }
