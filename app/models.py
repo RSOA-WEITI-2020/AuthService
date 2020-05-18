eb52794bb6ff8e9d9d6c1cbac85568a5b78a14b9
Wojciech Gruszka <wojciech@gruszka.eu>
2020-05-18 15:09:12 +0200

    Switched salt generation to bcrypt

--- app/models.py
+++ app/models.py
@@ -5,5 +5,4 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password_hash = db.Column(db.String(300), nullable=False)
-    password_salt = db.Column(db.String(16), nullable=False)
     email = db.Column(db.String(120), nullable=False)
