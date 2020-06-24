30f0942138a56426ec28afa39e3f04a688251cad
Jakub Gajownik <gajus123@gmail.com>
2020-06-25 00:35:31 +0200

    Fix test for password strength

--- app/test_resources.py
+++ app/test_resources.py
@@ -1,36 +1,52 @@
 import unittest
 import os
 import app
+import pytest
+
+app_dir = os.path.dirname(os.path.realpath(__file__))
+db_uri = 'sqlite:///' + app_dir + '/testing_db.db'
+try:
+    os.remove(app_dir+'/testing_db.db')
+except:
+    print('db file not found')
+flask_app = app.create_app(db_uri, app_dir + '/../keys')
 
 
-class RegistrationTest(unittest.TestCase):
+@pytest.fixture
+def app_fixture():
+    with flask_app.test_client() as client:
+        yield client
 
-    def setUp(self):
-        app_dir = os.path.dirname(os.path.realpath(__file__))
-        db_uri = 'sqlite:///' + app_dir + '/testing_db.db'
-        try:
-            os.remove(app_dir+'/testing_db.db')
-        except:
-            print('db file not found')
-        self.app = app.create_app(db_uri, app_dir + '/../keys').test_client()
+    app_dir = os.path.dirname(os.path.realpath(__file__))
+    try:
+        os.remove(app_dir+'/testing_db.db')
+    except:
+        print('db file not found')
 
-    def test_should_success_registration(self):
-        payload = {
-            "password": "abc1234",
-            "email": "test@test.com",
-            "first_name": "abc",
-            "last_name": "def",
-            "address": "ghi"
-        }
 
-        response = self.app.post('/v1/register', data=payload)
+def test_should_success_registration(app_fixture):
+    payload = {
+        "password": "super_trudne!haslo!oneone",
+        "email": "test@test.com",
+        "first_name": "abc",
+        "last_name": "def",
+        "address": "ghi"
+    }
 
-        self.assertEqual('ok', response.json['message'])
-        self.assertEqual(200, response.status_code)
+    response = app_fixture.post('/v1/register', data=payload)
 
-    def tearDown(self):
-        app_dir = os.path.dirname(os.path.realpath(__file__))
-        try:
-            os.remove(app_dir+'/testing_db.db')
-        except:
-            print('db file not found')
+    assert 'ok' == response.json['message']
+    assert 200 == response.status_code
+
+def test_password_not_strong_enough(app_fixture):
+    payload = {
+        "password": "abc1234",
+        "email": "test@test.com",
+        "first_name": "abc",
+        "last_name": "def",
+        "address": "ghi"
+    }
+
+    response = app_fixture.post('/v1/register', data=payload)
+
+    assert 400 == response.status_code
